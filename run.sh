bcd77b4b8a38a84ad5597cfbea98cc69309d5669
soylent-grin <hocico16@gmail.com>
2015-03-22 22:46:28 +0300

    Added defaul simulator config URL

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-APP_DIR=./simulator
 APP_NAME=simulator-heat-meter-1.0-SNAPSHOT
-
-pushd $APP_DIR
+SIMULATOR_CONFIG=https://raw.githubusercontent.com/semiotproject/simulator-heat-meters/master/simulatorConfig.xml
 
 git pull
 
@@ -11,9 +9,7 @@ mvn clean package -DskipTests=true
 
 wget ${SIMULATOR_CONFIG} -O ./simulatorConfig.xml
 
-java -jar ./target/${APP_NAME}.jar simulatorConfig.xml
-
-popd
+java -jar /root/simulator/target/${APP_NAME}.jar simulatorConfig.xml
 	
 
 
